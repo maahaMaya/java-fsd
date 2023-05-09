f2b7d990e1c4507f3e74bc5dbc147072b046930f
SOURAV <devbhsourav@gmail.com>
2023-05-09 10:58:24 +0530

    [sourav]add : assistedProject_phaseOne

---
+++ PhaseOne_AssistedPratice/phaseOneProgam/TypeCasting.java
@@ -0,0 +1,33 @@
+package phaseOneProgam;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// Implict Type Casting Example
+		int a = 12;
+		float b = a;
+		System.out.println("Int to Float (Implict Type Casting) : " + b);
+		
+		short c = 12;
+		int d = c;
+		System.out.println("short to int (Implict Type Casting) : " + d);
+		
+		// Explict Type Casting Example
+		
+		float f = 12.92f;
+		int g = (int) f;
+		System.out.println("float to int (Explict Type Casting) : " + g);
+		
+		char h = 'A';
+		int i = (int) h;
+		System.out.println("float to int (Explict Type Casting) : " + i);
+	}
+}
+
+
+/*
+ * //Output 
+ * Int to Float (Implict Type Casting) : 12.0 short to int (Implict
+ * Type Casting) : 12 float to int (Explict Type Casting) : 12 float to int
+ * (Explict Type Casting) : 65
+ */
