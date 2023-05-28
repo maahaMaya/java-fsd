e2eb92690df2f0a95e6b0a2100835ba15a8c9a20
sourav <devbhsourav@gmail.com>
2023-05-28 18:28:13 +0530

    [sourav]add: validateUser

---
+++ PhaseTwo_AssistedProject/ValidateUser/src/main/java/Login.java
@@ -0,0 +1,67 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("service methods call");
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out  = response.getWriter();
+		
+		String pname = request.getParameter("txtPname");
+		
+		
+		String pwd = request.getParameter("txtPwd");
+		
+		if(pname.equals("sk") && pwd.equals("123")) {
+			response.sendRedirect("WelcomePage.html");
+		}
+		else {
+			response.sendRedirect("ErrorPage.html");
+		}
+	}
+
+}
