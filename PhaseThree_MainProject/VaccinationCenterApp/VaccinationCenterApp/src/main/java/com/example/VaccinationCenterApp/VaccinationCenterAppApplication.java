afc3651c21b6281c03ec5f038bb623c2d8eba739
sourav <devbhsourav@gmail.com>
2023-06-18 23:07:48 +0530

    [sourav]add : FolderStructure

---
+++ PhaseThree_MainProject/VaccinationCenterApp/VaccinationCenterApp/src/main/java/com/example/VaccinationCenterApp/VaccinationCenterAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.VaccinationCenterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationCenterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationCenterAppApplication.class, args);
+	}
+
+}
