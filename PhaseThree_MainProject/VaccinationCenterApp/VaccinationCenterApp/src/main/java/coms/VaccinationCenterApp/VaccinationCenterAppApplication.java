eb51708ec8f6b4bbed5a343760096920529d3742
sourav <devbhsourav@gmail.com>
2023-06-19 21:07:43 +0530

    [sourav]modified : folderStructure

--- PhaseThree_MainProject/VaccinationCenterApp/VaccinationCenterApp/src/main/java/coms/VaccinationCenterApp/VaccinationCenterAppApplication.java
+++ PhaseThree_MainProject/VaccinationCenterApp/VaccinationCenterApp/src/main/java/coms/VaccinationCenterApp/VaccinationCenterAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.VaccinationCenterApp;
+package coms.VaccinationCenterApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- PhaseThree_MainProject/VaccinationCenterApp/VaccinationCenterApp/src/test/java/coms/VaccinationCenterApp/VaccinationCenterAppApplicationTests.java
+++ PhaseThree_MainProject/VaccinationCenterApp/VaccinationCenterApp/src/test/java/coms/VaccinationCenterApp/VaccinationCenterAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.VaccinationCenterApp;
+package coms.VaccinationCenterApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
